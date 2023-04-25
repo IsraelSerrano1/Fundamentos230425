384a42f112b6019a7bd0f643a651b378eb0d735f
IsraelSerrano1 <israel.serrano@softtek.com>
2023-04-25 17:21:02 +0200

    implemento subscribe

--- src/app/modificar-cursos/modificar-cursos.component.ts
+++ src/app/modificar-cursos/modificar-cursos.component.ts
@@ -6,25 +6,30 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-modificar-cursos',
   templateUrl: './modificar-cursos.component.html',
-  styleUrls: ['./modificar-cursos.component.css']
+  styleUrls: ['./modificar-cursos.component.css'],
 })
 export class ModificarCursosComponent {
-  curso: Curso = new Curso(0,"",0,0);
-  cursos: Curso[]=[];
+  curso: Curso = new Curso(0, '', 0, 0);
+  cursos: Curso[] = [];
 
-  constructor(private cursosService: ServicioCursosService, private activarRutas: ActivatedRoute){
-    this.cursos = this.cursosService.cursosS
-  }
-  
-  updateCurso(){
-    this.cursosService.updateCursoS(this.curso.id,this.curso)    
-  }
-  
-  ngOnInit(){
-    this.curso.id = this.activarRutas.snapshot.params['id'] 
-    if(this.curso.id != undefined){
-    this.curso = this.cursosService.encontrarCurso(this.curso.id)
-    }
+  constructor(
+    private cursosService: ServicioCursosService,
+    private activarRutas: ActivatedRoute
+  ) {
+    this.cursos = this.cursosService.cursosS;
   }
 
+  updateCurso() {
+    this.cursosService.updateCursoS(this.curso.id, this.curso);
+  }
+
+  ngOnInit() {
+    //this.curso.id= this.activarRutas.params.snapshot.params['id']
+    this.activarRutas.params.subscribe((data) => {
+      let idaux = data['id'];
+      if (idaux != undefined) {
+        this.curso = this.cursosService.encontrarCurso(idaux);
+      }
+    });
+  }
 }
